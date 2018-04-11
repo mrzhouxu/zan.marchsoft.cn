2f23b92dcf093a1adc9bff6c51d7b10aba162bd0
sangjinchao <603336339@qq.com>
2018-04-11 14:47:24 +0800

    update module

---
+++ src/mobile/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router' 
+
+import axios from 'axios'
+window.axios = axios
+
+import Mock from 'mockjs'
+window.Mock = Mock
+require('../../util/mock/mobile_mock')
+
+// 引入全部组件
+import Mint from 'mint-ui';
+Vue.use(Mint);
+import 'mint-ui/lib/style.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
